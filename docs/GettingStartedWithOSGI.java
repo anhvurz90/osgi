f73484429968e460b3001edb045307c2cea19bbe
Nguyen Anh Vu <anhvurz90@gmail.com>
2017-09-22 21:28:58 +0700

    Add Getting started with OSGi: Your first bundle: docs

--- docs/GettingStartedWithOSGI.java
+++ docs/GettingStartedWithOSGI.java
@@ -3,10 +3,10 @@ Link: https://www.osgi.org/developer/resources/learning-resources-tutorials/tuto
   - Link: https://www.eclipsezone.com//eclipse/forums/m92130843.html
   - Run: java -Dosgi.noshutdown=true -Dosgi.console.enable.builtin=true -jar org.eclipse.osgi_3.9.1.v20140110-1610.jar -console
   - ss: short status
-  - HelloActivator: {
-    import org.osgi.framework.*;
+  - HelloActivator.java: {
+      import org.osgi.framework.*;
  
-    public class HelloActivator implements BundleActivator {
+      public class HelloActivator implements BundleActivator {
     	public void start(BundleContext context) {
     	    System.out.println("Hello EclipseZone Readers!");
   	}
@@ -14,7 +14,24 @@ Link: https://www.osgi.org/developer/resources/learning-resources-tutorials/tuto
   	public void stop(BundleContext context) {
     	    System.out.println("Goodbye EclipseZone Readers!");
   	}
+      }
     }
-  } 
+  - HelloWorld.mf: {
+        Manifest-Version: 1.0
+	Bundle-Name: HelloWorld
+	Bundle-Activator: HelloActivator
+	Bundle-SymbolicName: HelloWorld
+	Bundle-Version: 1.0.0
+	Import-Package: org.osgi.framework
+    }
+  - Compile & Build: {
+	> javac -classpath equinox.jar HelloActivator.java
+	> jar -cfm HelloWorld.jar HelloWorld.mf HelloActivator.class
+  }
+  - install: {
+    	> install file:HelloWorld.jar
+  }
+  - start: {
+	start 1
+  }
 }
-
