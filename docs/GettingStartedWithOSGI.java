cc7ce101cc155b017324306204bb605054b54cdf
Nguyen Anh Vu <anhvurz90@gmail.com>
2017-09-22 21:31:54 +0700

    Add Getting started with OSGi: Interacting with the Framework: docs

--- docs/GettingStartedWithOSGI.java
+++ docs/GettingStartedWithOSGI.java
@@ -35,3 +35,51 @@ Link: https://www.osgi.org/developer/resources/learning-resources-tutorials/tuto
 	start 1
   }
 }
+2.Interacting with the Framework: {
+  - HelloWorldKiller.java: {
+	import org.osgi.framework.*;
+	public class HelloWorldKiller implements BundleActivator {
+		public void start(BundleContext context) {
+			System.out.println("HelloWorldKiller searching...");
+			Bundle[] bundles = context.getBundles();
+			for(int i=0; i<bundles.length; i++) {
+				if("HelloWorld".equals(bundles[i]
+						      .getSymbolicName())) {
+					try {
+					    System.out.println("Hello World found, "
+							     + "destroying!");
+					    bundles[i].uninstall();
+					    return;
+					} catch (BundleException e) {
+					    System.err.println("Failed: "
+							     + e.getMessage());
+					}
+				}
+			}
+			System.out.println("Hello World bundle not found");
+		}
+ 
+		public void stop(BundleContext context) {
+			System.out.println("HelloWorldKiller shutting down");
+		}
+	}
+  }
+  - HelloWorldKiller.mf: {
+    	Manifest-Version: 1.0
+	Bundle-Name: HelloWorldKiller
+	Bundle-Activator: HelloWorldKiller
+	Bundle-SymbolicName: HelloWorldKiller
+	Bundle-Version: 1.0.0
+	Import-Package: org.osgi.framework
+  }
+  - Compile & Build: {
+	> javac -classpath equinox.jar HelloWorldKiller.java
+	> jar -cfm HelloWorldKiller.jar HelloWorldKiller.mf HelloWorldKiller.class
+  }
+  - install: {
+	install file:HelloWorldKiller.jar
+  }
+  - start: {
+	start 2
+  }
+}
