19c2afe964d4768a5f29d2962efe90c3d4eb2ec3
Nguyen Anh Vu <anhvurz90@gmail.com>
2017-09-22 21:06:57 +0700

    Add links.java

---
+++ docs/links.java
@@ -0,0 +1,8 @@
+Links:
+1. https://www.osgi.org/developer/architecture/
+2. https://www.osgi.org/developer/where-to-start/
+3. https://www.osgi.org/developer/resources/learning-resources-tutorials/tutorial-archive/
+4. https://www.osgi.org/developer/resources/learning-resources-tutorials/
+5. http://enroute.osgi.org/
+6. http://enroute.osgi.org/book/150-tutorials.html
+7. http://enroute.osgi.org/book/180-examples.html
